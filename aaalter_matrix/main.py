467bccc56dcde12e29ba648ae30ee7548ab4a285
myOmikron <accounts@omikron.dev>
2020-11-10 00:23:12 +0100

    Added check for required parameter in config

--- aaalter_matrix/main.py
+++ aaalter_matrix/main.py
@@ -9,12 +9,14 @@ def load_config():
         except JSONDecodeError as err:
             print(f"Error while parsing config:{err.msg}")
             exit(1)
+    if not all(x in config for x in ["homeserver_url", "room_id", "user_name", "user_pass"]):
+        print("Missing required parameter!")
+        exit(1)
     return config
 
 
 def main():
     config = load_config()
-    print(config)
 
 
 if __name__ == '__main__':
